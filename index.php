ac70af607e7f47fd078abd6515c526c1735f5eef
Tirguy <matooparis14@gmail.com>
2018-05-08 22:34:54 +0200

    version 0.2

--- index.php
+++ index.php
@@ -3,6 +3,49 @@
 <head>
 <meta charset="utf-8">
 <title>IoT Syslog : Accueil</title>
+<style type="text/css">
+<!--
+
+/* BEGIN Strengthening the lins */
+ #navlist
+{
+padding: 0 1px 1px;
+margin-left: 0;
+font: bold 12px Verdana, sans-serif;
+background: gray;
+width: 13em;
+}
+
+#navlist li
+{
+list-style: none;
+margin: 0;
+border-top: 1px solid gray;
+text-align: left;
+}
+
+#navlist li a
+{
+display: block;
+padding: 0.25em 0.5em 0.25em 0.75em;
+border-left: 1em solid #AAB;
+background: #CCD;
+text-decoration: none;
+}
+
+#navlist li a:link { color: #448; }
+#navlist li a:visited { color: #667; }
+
+#navlist li a:hover
+{
+border-color: #FE3;
+color: #FFF;
+background: #332;
+}
+
+/* END Strengthening the lins */
+-->
+</style>
 </head>
 
 <body id="index">
@@ -15,6 +58,11 @@
 <p>Voir les LOG d'un appareil</p>
 <p>Voir les LOG d'une adresse IP </p>
 <p>Répartition du nombre de LOG par machine (camembert)</p>
+<div id="navcontainer">
+  <ul id="navlist">
+    <li><a href="#" title="test">test</a></li>
+  </ul>
+</div>
 <p>&nbsp;</p>
 </body>
 </html>
